ced2dea99e3f2b480f4b736bc1779f57fa30ec60
Nikita <titata1@mail.ru>
2021-03-13 21:00:23 +0300

    feat: add func revers number

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
 module.exports = function reverse (n) {
-  
+    str=String(Math.abs(n));
+    var str_array=str.split("");
+    str_array=str_array.reverse();
+    str_array=str_array.join("");
+    return Number(str_array);
 }
